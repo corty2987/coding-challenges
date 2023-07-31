7fff67afd930d7d8cb6e7be15e1c3e177c413de6
Courtney Blackwell <125604584+corty2987@users.noreply.github.com>
2023-07-31 12:57:00 -0500

    Update Week4_Challenge.js

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,16 @@
 
+function addUp(num) {
+	let nums = [];
+	nums 
+}
+
+//Create a function that takes a number as an argument. 
+//Add up all the numbers from 1 to the number you passed to the function. 
+//For example, if the input is 4 then your function should return 10 because 
+//1 + 2 + 3 + 4 = 10.
+
+//create an array that takes in numbers
+//decrement?? i--?
+//print every number less than the number entered down to and including 1
+//add the numbers printed for the result
+//return result
